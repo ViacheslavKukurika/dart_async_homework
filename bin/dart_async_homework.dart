778d54a74fe31d89936f99b821c49e1412c31cb3
Viacheslav <viacheslavkukurika@gmail.com>
2026-02-20 21:55:39 +0200

    Second task completed

--- bin/dart_async_homework.dart
+++ bin/dart_async_homework.dart
@@ -1,8 +1,15 @@
 import 'package:dart_async_homework/hw_futures.dart';
 
-  // Task 1: Асинхронне отримання імені
+// Task 1: Асинхронне отримання імені
 
-  Future<void> main() async {
-    final name = await fetchName();
-    print('Мене звати $name');
+Future<void> main() async {
+  final name = await fetchName();
+  print('Мене звати $name');
+
+  // Task 2: Асинхронне отримання віку
+
+  final ageStr = await fetchAge();
+  final age = int.parse(ageStr);
+
+  print('Мені $age ${ageWord(age)}');
 }
--- lib/hw_futures.dart.dart
+++ lib/hw_futures.dart.dart
@@ -6,3 +6,24 @@ Future<String> fetchName() async {
   return 'Viacheslav';
 }
 
+// Task 2 - 4
+Future<String> fetchAge() async {
+  await Future.delayed(Duration(milliseconds: 1500));
+  return '25';
+}
+
+String ageWord(int age) {
+  final lastTwo = age % 100;
+  final last = age % 10;
+
+  if (lastTwo >= 11 && lastTwo <= 14) {
+    return 'років';
+  } else if (last == 1) {
+    return 'рік';
+  } else if (last >= 2 && last <= 4) {
+    return 'роки';
+  } else {
+    return 'років';
+  }
+}
+
